211c558b8c48eb87bc0f7999063f38548d0fa010
Anton Bayborosha <anton.bayborosha@gmail.com>
2023-02-24 10:31:02 +0100

    cleaning commit

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,25 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to movies_path(@movie)
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :poster_url, :rating)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/movies', to: 'movies#index', as: :movies
+
+  get '/movies/new', to: 'movies#new', as: :new_movie
+  patch '/movies', to: 'movies#create'
+
+  get '/movies/:id', to: 'movies#show', as: :movie
 end
